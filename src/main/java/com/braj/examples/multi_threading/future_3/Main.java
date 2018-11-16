a432290364c377baf64b265c9a13f728c8f0d7a9
bijeesraj <bijeesraj007@gmx.com>
2018-11-16 08:03:55 -0600

    cleanup

--- src/main/java/com/braj/examples/multi_threading/future_3/Main.java
+++ src/main/java/com/braj/examples/multi_threading/future_3/Main.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-public class FutureExampleMain {
+public class Main {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService threadPool = Executors.newFixedThreadPool(50);
@@ -13,8 +13,8 @@ public class FutureExampleMain {
         try {
             long start = System.currentTimeMillis();
             //Both below statements will be running in parallel.
-            Future<String> randomFutureString = threadPool.submit(new RandomStringGenerator());
-            Future<Integer> randomFutureNumber = threadPool.submit(new RandomNumberGenerator());
+            Future<String> randomFutureString = threadPool.submit(new RandomStringGenerator()); // task takes 5 Sec
+            Future<Integer> randomFutureNumber = threadPool.submit(new RandomNumberGenerator()); // task takes 3 second
             System.out.println("Initiated to generated RandomString and RandomNumber");
 
             // get() on future is the only blocking call.
@@ -23,7 +23,7 @@ public class FutureExampleMain {
 
             long elapsedTime = System.currentTimeMillis() - start;
             System.out.println("Elapsed Time :" + elapsedTime);
-            // Note that though 5 sec + 3 sec is the execution time for each method , the elasped time is only 5 sec (NOT 8 Sec).
+            // Note that though 5 sec + 3 sec is the execution time for each method , the elapsed time is only 5 sec (NOT 8 Sec).
 
         }finally {
             threadPool.shutdown(); // Make sure to close once done

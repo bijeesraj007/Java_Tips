3076a5bca382597a4812a1c35cba75e7d4474cb9
bijeesraj <bijeesraj007@gmx.com>
2018-11-17 15:51:59 -0600

    FlatMap Update

--- src/main/java/com/braj/examples/java8map/MapAndFlatMap.java
+++ src/main/java/com/braj/examples/java8map/MapAndFlatMap.java
@@ -9,25 +9,29 @@ import java.util.stream.Stream;
 
 public class MapAndFlatMap {
 
-    public static void main(String[] args) {
+    // Flat map is used to flatten out a structure.
 
+    public static void main(String[] args) {
+        mapAndFlatMapexample1();
+
+    }
+
+    private static void mapAndFlatMapexample1() {
         // 1 : map works well with simple and direct mappings
         List<String> myList = Stream.of("a", "b")
                 .map(String::toUpperCase)
                 .collect(Collectors.toList());
-        System.out.println(myList);
+        System.out.println("With Using Map :" + myList);
 
         //2 : flatmap shouldbe used for complex mapping
-        List<List<String>> complexList = Arrays.asList(
-                                                        Arrays.asList("a"),
+        // Note that here its a List of List of String... But we flattened out to a normal form
+        List<List<String>> complexList = Arrays.asList( Arrays.asList("a"),
                                                         Arrays.asList("b"));
-        //System.out.println(complexList);
 
-        System.out.println(complexList
-                .stream()
-                .flatMap(Collection::stream)
-                .map(String::toUpperCase)
-                .collect(Collectors.toList()));
-
+        System.out.println("With Using Flat Map :" +complexList
+                                                .stream()
+                                                .flatMap(Collection::stream) // Flattening
+                                                .map(String::toUpperCase)
+                                                .collect(Collectors.toList()));
     }
 }

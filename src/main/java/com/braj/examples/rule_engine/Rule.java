7c7cbc858637c8357e9eb42530f3e0791e4f6544
bijeesraj <bijeesraj007@gmx.com>
2019-07-26 19:29:52 -0500

    comment

--- src/main/java/com/braj/examples/rule_engine/Rule.java
+++ src/main/java/com/braj/examples/rule_engine/Rule.java
@@ -2,6 +2,6 @@ package com.braj.examples.rule_engine;
 
 public interface Rule <I,O> {
 
-    boolean matches(I input); // Added to avoid "if check of phone type" so that it can be used in functional way.
-    O applyRule(I input);
+    boolean matches(I input); // Added to avoid "if check of phone type" (see that in Main_TraditionalWay1.java) so that it can be used in functional way.
+    O applyRule(I input); // apply rule/logic on the i/p obj and produce o/p obj
 }
